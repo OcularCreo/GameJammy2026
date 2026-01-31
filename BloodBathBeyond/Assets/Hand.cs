6e6c4078ed35d93a3dfd4bb66949fd06016500a0
hujillian <hujillian123@gmail.com>
2026-01-31 14:06:57 -0500

    Set cursor to image

--- BloodBathBeyond/Assets/Hand.cs
+++ BloodBathBeyond/Assets/Hand.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Hand : MonoBehaviour
 {
+    private Vector3 mousePosition;
+    [SerializeField] Texture2D cursor;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,11 @@ public class Hand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //mousePosition = Input.mousePosition;
+        //mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //transform.position = new Vector3(mousePosition.x, mousePosition.y, 0f);
+
+
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.ForceSoftware);
     }
 }
