3fb26f82e1b5d0abef64bf14d1fc96d9366ba631
OcularCreo <122617813+OcularCreo@users.noreply.github.com>
2026-01-31 13:29:17 -0500

    created gameManager and vampire script

---
+++ BloodBathBeyond/Assets/Scripts/gameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public enum Items { cucumber, orange, pMask, gMask, towel };
+
+public class gameManager : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
